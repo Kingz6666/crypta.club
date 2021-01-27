5074a7e2d7405c435b790f37637c2ad9781d2b54
state <66269438+Kingz6666@users.noreply.github.com>
2021-01-27 18:35:05 +0200

    Improved -_-----

--- Resolver.cpp
+++ Resolver.cpp
@@ -1,9 +1,12 @@
 // TESTING.
-
+// PROJECT NOT SDK ITS GIDE FOR NN CODERS.
 float new_body_yaw = 0.0f;
 auto diff_yaw = math::angle_difference(player->eye_angles().y, player->anim_state()->goal_feet_yaw;
 
 if ( diff_yaw < 2.0f && !new_body_yaw < 0.0f )
 {
-return;
+new_body_yaw = diff_yaw / animstate->pad10[512]; // min body yaw.
+}
+{
+new_body_yaw = diff_yaw / animstate->pad10[516]; // max body yaw.
 }
