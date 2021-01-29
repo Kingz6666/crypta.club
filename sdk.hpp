36cc98bb396339026b22d68d646b1dfdbd110bec
state <66269438+Kingz6666@users.noreply.github.com>
2021-01-29 15:29:40 +0200

    create classes [ animstate ] / [ interpalation ]

---
+++ sdk.hpp
@@ -0,0 +1,15 @@
+class AnimState 
+{
+float abs_yaw; // 0x49
+float goal_feet_yaw; // unknwon offset.
+//char pad10; // 0x34
+int detect_move; // 0x0
+float max_yaw; // 0x14
+float min_yaw; // 0x13
+float lby_state; // 0x15
+}
+
+class interpalation_info
+{
+float effect_info; // 0x10
+}
