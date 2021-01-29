38c6f4e4a83c72adcda5f51c083d200d6341c604
state <66269438+Kingz6666@users.noreply.github.com>
2021-01-29 15:44:26 +0200

    smail fixes.

--- Resolver.cpp
+++ Resolver.cpp
@@ -42,10 +42,10 @@ void next_side()
   {
    next_side_time = get_globals()->m_crutime; // apply switch desync or break stand lower body yaw. 
   }
-/*  else
+  else
   {
-   player->lower_body_yaw() == animstate->max_yaw && !player->lower_body_yaw();
-  } */
+   player->lower_body_yaw()/* == animstate->max_yaw && !player->lower_body_yaw()*/;
+  }/
   
 /*  if ( get_globals()->m_curtime < 0.0f )
   {
