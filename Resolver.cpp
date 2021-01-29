a529828afd1c1a974411b52ccfad75f0988cc95c
state <66269438+Kingz6666@users.noreply.github.com>
2021-01-29 15:38:43 +0200

    Update Resolver.cpp

--- Resolver.cpp
+++ Resolver.cpp
@@ -18,22 +18,18 @@ new_body_yaw = diff_yaw / animstate->pad10[512]; // min body yaw.
 new_body_yaw = diff_yaw / animstate->pad10[516]; // max body yaw.
 }
 
-if ( diff_yaw > 58 && diff_yaw == -58 )
+/*if ( diff_yaw > 58 && diff_yaw == -58 )
 {
 return new_body_yaw;
 }
 else
 {
  new_body_yaw =  animstate->goal_feet_yaw;
-}
+}*/
 
 if ( animstate->abs_yaw != 0.0 )
 {
-animstate->goal_feet_yaw = diff_yaw / 2;
-}
-else
-{
-animstate->goal_feet_yaw = diff_yaw / 1;//return animstate->abs_yaw;
+animstate->goal_feet_yaw = diff_yaw / next_side();
 }
 }
 
