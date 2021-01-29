f9c675e624a651d8ac2c2f16f0f70b697db7c5eb
state <66269438+Kingz6666@users.noreply.github.com>
2021-01-29 15:01:16 +0200

    added check on fake lags.

---
+++ ServerSide.cpp
@@ -0,0 +1,11 @@
+void server_side::ServerSide(float side)
+{
+if ( fakelags::get().max_choke > 0 )
+{
+fakelags::get().max_choke > 16; // max choke.
+}
+else
+{
+fakelags::get().max_choke = 0;
+}
+}
